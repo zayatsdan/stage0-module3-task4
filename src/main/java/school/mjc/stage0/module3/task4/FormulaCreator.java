f658f00deb57a6fa2d53a2e320d35ee61bda7972
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-16 12:00:59 +0100

    Update FormulaCreator.java

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a;
+        double b;
+        String formula=String.format("(9*a^2 - 5*b + 2 + a - 7) * ((a + b - 4*a*b) / 2)");
+        System.out.println(formula);
     }
 }
