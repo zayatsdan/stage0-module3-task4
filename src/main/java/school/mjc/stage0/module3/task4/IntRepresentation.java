91e7972315943b178ec6010556d4679e6252be13
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-16 11:58:01 +0100

    Update IntRepresentation.java

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char z= 'c';
+        char x= 'h';
+        char y= 'a';
+        char v= 'r';
+        int c=(int)z;
+        int h=(int)x;
+        int a=(int)y;
+        int r=(int)v;
+        System.out.println(c + "\n" +h + "\n"+a + "\n"+r);
     }
 }
