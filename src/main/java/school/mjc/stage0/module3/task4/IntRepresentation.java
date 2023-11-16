de2904c85fd8909dd9c42ec739d9ca72370aebf3
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-16 17:50:09 +0100

    Update IntRepresentation.java

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,14 +2,17 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        char z= 'c';
-        char x= 'h';
-        char y= 'a';
-        char v= 'r';
-        int c=(int)z;
-        int h=(int)x;
-        int a=(int)y;
-        int r=(int)v;
-        System.out.println(c + h + a + r);
+         char char1 = 'c';
+        char char2 = 'h';
+        char char3 = 'a';
+        char char4 = 'r';
+
+        int intRepresentation1 = (int) char1;
+        int intRepresentation2 = (int) char2;
+        int intRepresentation3 = (int) char3;
+        int intRepresentation4 = (int) char4;
+
+        System.out.println(intRepresentation1 + " " + intRepresentation2 + " " + intRepresentation3 + " " + intRepresentation4);
+    }
     }
 }
